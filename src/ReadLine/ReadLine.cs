fef894dd097cc1fd3d469c1ebf5a917524aa6e66
Christian Hood <christianhood@gmail.com>
2021-08-09 14:46:51 -0600

    Add async methods ReadAsync and ReadPasswordAsync that take in a CancellationToken

--- src/ReadLine/ReadLine.cs
+++ src/ReadLine/ReadLine.cs
@@ -2,6 +2,8 @@
 using Internal.ReadLine.Abstractions;
 
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace System
 {
@@ -39,11 +41,42 @@ namespace System
             return text;
         }
 
+        public static async Task<string> ReadAsync(string prompt = "", string @default = "", CancellationToken cancellationToken = default(CancellationToken))
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            Console.Write(prompt);
+            KeyHandler keyHandler = new KeyHandler(new Console2(), _history, AutoCompletionHandler);
+            string text = await GetTextAsync(keyHandler, cancellationToken);
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (String.IsNullOrWhiteSpace(text) && !String.IsNullOrWhiteSpace(@default))
+            {
+                text = @default;
+            }
+            else
+            {
+                if (HistoryEnabled)
+                    _history.Add(text);
+            }
+
+            return text;
+        }
+
         public static string ReadPassword(string prompt = "")
         {
             Console.Write(prompt);
             KeyHandler keyHandler = new KeyHandler(new Console2() { PasswordMode = true }, null, null);
             return GetText(keyHandler);
+        }
+
+        public static Task<string> ReadPasswordAsync(string prompt = "", CancellationToken cancellationToken = default(CancellationToken))
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            Console.Write(prompt);
+            KeyHandler keyHandler = new KeyHandler(new Console2() { PasswordMode = true }, null, null);
+            return GetTextAsync(keyHandler, cancellationToken);
         }
 
         private static string GetText(KeyHandler keyHandler)
@@ -55,6 +88,29 @@ namespace System
                 keyInfo = Console.ReadKey(true);
             }
 
+            Console.WriteLine();
+            return keyHandler.Text;
+        }
+
+        private static async Task<string> GetTextAsync(KeyHandler keyHandler, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            while (!Console.KeyAvailable && !cancellationToken.IsCancellationRequested)
+                await Task.Delay(50, cancellationToken);
+            cancellationToken.ThrowIfCancellationRequested();
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+            while (keyInfo.Key != ConsoleKey.Enter)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                keyHandler.Handle(keyInfo);
+
+                while (!Console.KeyAvailable && !cancellationToken.IsCancellationRequested)
+                    await Task.Delay(50, cancellationToken);
+                cancellationToken.ThrowIfCancellationRequested();
+                keyInfo = Console.ReadKey(true);
+            }
+
+            cancellationToken.ThrowIfCancellationRequested();
             Console.WriteLine();
             return keyHandler.Text;
         }
